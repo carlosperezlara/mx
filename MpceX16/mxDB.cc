7bbb76ee2061911fa3364d404945134bebe705d0
Carlos Perez Lara <cperez@rcas2066.rcf.bnl.gov>
2017-11-13 10:08:14 -0500

    fixes

--- MpceX16/mxDB.cc
+++ MpceX16/mxDB.cc
@@ -2,29 +2,16 @@
 #include "mxDB.exlan.cc"
 #include "mxDB.exl2h.cc"
 #include "mxDB.mpctau.cc"
-#include "mxDB.exlanx.cc"
-#include "mxDB.exrun.cc"
+//#include "mxDB.exrun.cc"
 #include <fstream>
 
 namespace mxDB {
-  bool loadextras = false;
   void read(int run, mxCalibMaster *cal) {
     std::cout << "Reading static table for " << run << std::endl;
     exped(run,cal);
     exhlf(run,cal);
     exlan(run,cal);
-    //=====
-    if(mxDB::loadextras) {
-      exlsgm(run,cal);
-      exla(run,cal);
-      exlf2(run,cal);
-      exlf3(run,cal);
-      exlf4(run,cal);
-      exlb(run,cal);
-      exlsl(run,cal);
-    }
-    //=====
-    exrun(run,cal);
+    //exrun(run,cal);
     //=====
     mpctau(run,cal);
     std::ifstream ifile("THEbadkeys.dat");
